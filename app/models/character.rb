ed8bca899bb280477bc8fd29777687959031cacc
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-20 21:48:00 -0400

    struggles

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def actor
+    actor_id = self.actor_id
+    Actor.find(actor_id)
+  end
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,8 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :shows
+  belongs_to :network
+  def network
+    Network.find(self.network_id)
+  end
 end
